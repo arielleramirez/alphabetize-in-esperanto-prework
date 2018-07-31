0067ff1dffefe87ece4f2d0a3772d8380cb50457
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-31 09:17:07 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
-def alphabetize(arr)
-  # code here
-end
+def alphabetize(arr)	def alphabetize(arr)
+  # code here	  arr.sort_by! do |a|
+    positions = []
+    a.each_char do |char|
+      positions << ESPERANTO_ALPHABET.index(char)
+    end
+    positions
+  end
+  arr
+end 	end 
